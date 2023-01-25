d650af12b66a9c24c559c05d3cee09688b232d2e
Pythonic-Rainbow <wongchingho0@gmail.com>
2023-01-25 21:58:27 +0000

    [Algo:Search] Binary Search

--- algo/search.py
+++ algo/search.py
@@ -2,3 +2,16 @@ def linear_search(seq, target):
     for i, obj in enumerate(seq):
         if obj == target:
             return i
+
+
+def binary_search(seq, target):
+    low, high = 0, len(seq)-1
+    mid = (low+high) // 2
+    while mid < high:
+        if seq[mid] == target:
+            return mid
+        if seq[mid] < target:
+            low = mid+1
+        else:
+            high = mid
+        mid = (low+high) // 2
